d05bed0520509fb04279ea4f8700f9863f2e15d8
EuJ <pzweuj@live.com>
2024-12-13 17:11:37 +0800

    Update cnvanno.py

--- cnvanno.py
+++ cnvanno.py
@@ -10,7 +10,7 @@ from src.database_prepare import prepare_all
 # 运行
 def main():
     parser = argparse.ArgumentParser(
-        description="使用示例：python3 cnvanno.py anno -i <input bed> -o <output txt> -g <hg19|hg38>",
+        description="Usage：python3 cnvanno.py anno -i <input bed> -o <output txt> -g <hg19|hg38>",
         prog="cnvanno.py",
         usage="python3 cnvanno.py -h",
         formatter_class=argparse.RawTextHelpFormatter
@@ -18,7 +18,7 @@ def main():
     parser.add_argument("-v", "--version", action="version", version="Version 0.1 20241213")
     parser.add_argument('option', type=str,
                         choices=["anno", "database"],
-                        help='\n可选运行步骤：\nanno      注释流程\ndatabase     数据库准备\n')
+                        help='\nOptions：\nanno      annotation mode\ndatabase     database prepare mode\n')
     
     if len(sys.argv[1:]) == 0:
         parser.print_help()
@@ -29,11 +29,11 @@ def main():
     option_set = args.option
     
     if option_set == "anno":
-        parser.add_argument("-i", "--input", type=str, help="输入bed文件，格式如chr1 100 200 DUP")
-        parser.add_argument("-o", "--output", type=str, help="输出结果txt文件")
-        parser.add_argument("-g", "--genome", type=str, help="参考基因组版本，默认hg19，可选[hg19, hg38]", default="hg19")
+        parser.add_argument("-i", "--input", type=str, help="input bed，format: chr1 100 200 DUP")
+        parser.add_argument("-o", "--output", type=str, help="output txt")
+        parser.add_argument("-g", "--genome", type=str, help="genome version，default is hg19，[hg19, hg38]", default="hg19")
     elif option_set == "database":
-        parser.add_argument("-g", "--genome", type=str, help="参考基因组版本，可选[hg19, hg38]")
+        parser.add_argument("-g", "--genome", type=str, help="genome version，[hg19, hg38]")
     
     if len(sys.argv[1:]) == 0:
         parser.print_help()
